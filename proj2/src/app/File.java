afdb548fea2c1898573e3e50a0bd2abbdad06cb6
harrydehix <harry.holly02@gmail.com>
2020-07-13 11:31:37 +0200

    Update jar-generation action

---
+++ proj2/src/app/File.java
@@ -0,0 +1,7 @@
+package app;
+
+public class File{
+	public static void main(String[] args){
+		System.out.println("Hallo Welt!");
+	}
+}
---
+++ proj2/src/app/lol/Lol.java
@@ -0,0 +1,9 @@
+package app.lol;
+
+public class Lol{
+	private static int c = 23;
+
+	public static void main(String[] args){
+		System.out.println(c);
+	}
+}
